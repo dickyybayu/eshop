31a05aed436e706f7a640561b9791c6d5c1967e6
dikbayy <dicky.bayusadewo@gmail.com>
2025-02-13 14:18:21 +0700

    Feat: Implement delete product functionality

--- src/main/java/id/ac/ui/cs/advprog/eshop/service/ProductService.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/service/ProductService.java
@@ -8,5 +8,6 @@ public interface ProductService {
     public List<Product> findAll();
     public Product findById(String productId);
     public Product update(Product product);
+    public void delete(Product product);
 
 }
