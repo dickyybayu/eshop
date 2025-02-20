fa75d0c6b5b219abd704d38485e47f0d5e9725db
dikbayy <dicky.bayusadewo@gmail.com>
2025-02-20 20:27:03 +0700

    Refactor: Replace field injection with constructor injection

--- src/main/java/id/ac/ui/cs/advprog/eshop/service/ProductServiceImpl.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/service/ProductServiceImpl.java
@@ -12,8 +12,12 @@ import java.util.UUID;
 
 @Service
 public class ProductServiceImpl implements ProductService {
-    @Autowired
-    private ProductRepository productRepository;
+    private final ProductRepository productRepository;
+
+    public ProductServiceImpl(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
     @Override
     public Product create(Product product) {
         productRepository.create(product);
