6601e0d1052a2b2d0e8342b624fab24bc0a597a8
dikbayy <dicky.bayusadewo@gmail.com>
2025-02-20 21:21:40 +0700

    Refactor: Group dependencies by destination to improve maintainability

--- build.gradle.kts
+++ build.gradle.kts
@@ -55,11 +55,11 @@ dependencies {
 
     // Test dependencies
     testImplementation("org.springframework.boot:spring-boot-starter-test")
-    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
     testImplementation("org.seleniumhq.selenium:selenium-java:$seleniumJavaVersion")
     testImplementation("io.github.bonigarcia:selenium-jupiter:$seleniumJupiterVersion")
     testImplementation("io.github.bonigarcia:webdrivermanager:$webdrivermanagerVersion")
     testImplementation("org.junit.jupiter:junit-jupiter:$junitJupiterVersion")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
 
