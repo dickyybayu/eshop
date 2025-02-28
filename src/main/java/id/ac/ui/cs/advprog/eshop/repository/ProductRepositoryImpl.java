05850ebe20d76e72c7e5c31e9d89295cb10c4bd3
dikbayy <dicky.bayusadewo@gmail.com>
2025-02-28 18:48:40 +0700

    Refactor: Convert ProductRepository to interfaces to apply DIP and OCP

--- src/main/java/id/ac/ui/cs/advprog/eshop/repository/ProductRepositoryImpl.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/repository/ProductRepositoryImpl.java
@@ -1,24 +1,26 @@
 package id.ac.ui.cs.advprog.eshop.repository;
+
 import id.ac.ui.cs.advprog.eshop.model.Product;
 import org.springframework.stereotype.Repository;
-
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 
 @Repository
-public class ProductRepository {
+public class ProductRepositoryImpl implements ProductRepository {
     private List<Product> productData = new ArrayList<>();
 
+    @Override
     public Product create(Product product) {
         productData.add(product);
         return product;
     }
 
-    public Iterator<Product> findAll() {
-        return productData.iterator();
+    @Override
+    public List<Product> findAll() {
+        return new ArrayList<>(productData);
     }
 
+    @Override
     public Product findById(String productId) {
         for (Product product: productData) {
             if (product.getProductId().equals(productId)) {
@@ -28,23 +30,21 @@ public class ProductRepository {
         return null;
     }
 
-    public Product update(Product product) {
+    @Override
+    public Product update(Product updatedProduct) {
         for (int i = 0; i < productData.size(); i++) {
-            if (productData.get(i).getProductId().equals(product.getProductId())) {
-                productData.set(i, product);
+            Product product = productData.get(i);
+            if (product.getProductId().equals(updatedProduct.getProductId())) {
+                product.setProductName(updatedProduct.getProductName());
+                product.setProductQuantity(updatedProduct.getProductQuantity());
                 return product;
             }
         }
         return null;
     }
 
+    @Override
     public void delete(Product product) {
-        for (int i = 0; i < productData.size(); i++) {
-            if (productData.get(i).getProductId().equals(product.getProductId())) {
-                productData.remove(i);
-                return;
-            }
-        }
+        productData.remove(product);
     }
-
 }
