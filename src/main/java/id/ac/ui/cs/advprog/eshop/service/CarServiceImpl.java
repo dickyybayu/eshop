e76383839b36713c5598fc0b6eebf709aa146945
dikbayy <dicky.bayusadewo@gmail.com>
2025-02-28 18:51:35 +0700

    Refactor: Minor updates to CarService and ProductService for improved dependency injection, applying DIP

--- src/main/java/id/ac/ui/cs/advprog/eshop/service/CarServiceImpl.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/service/CarServiceImpl.java
@@ -1,48 +1,42 @@
 package id.ac.ui.cs.advprog.eshop.service;
+
 import id.ac.ui.cs.advprog.eshop.model.Car;
 import id.ac.ui.cs.advprog.eshop.repository.CarRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import java.util.ArrayList;
-import java.util.Iterator;
+
 import java.util.List;
 
 @Service
 public class CarServiceImpl implements CarService {
 
-    @Autowired
-    private CarRepository carRepository;
+    private final CarRepository carRepository;
 
-    @Override
-    public Car create (Car car) {
-        // TODO Auto-generated method stub
-        carRepository.create(car);
-        return car;
+    public CarServiceImpl(CarRepository carRepository) {
+        this.carRepository = carRepository;
     }
 
     @Override
-    public List <Car> findAll() {
-        Iterator <Car> carIterator = carRepository.findAll();
-        List <Car> allCar = new ArrayList<>();
-        carIterator.forEachRemaining(allCar::add);
-        return allCar;
+    public Car create(Car car) {
+        return carRepository.create(car);
     }
 
     @Override
-    public Car findById (String carId) {
-        Car car = carRepository.findById(carId);
-        return car;
+    public List<Car> findAll() {
+        return carRepository.findAll();
     }
 
     @Override
-    public void update (String carId, Car car) {
-        // TODO Auto-generated method stub
+    public Car findById(String carId) {
+        return carRepository.findById(carId);
+    }
+
+    @Override
+    public void update(String carId, Car car) {
         carRepository.update(carId, car);
     }
 
     @Override
-    public void deleteCarById (String carId) {
-        // TODO Auto-generated method stub
+    public void deleteCarById(String carId) {
         carRepository.delete(carId);
     }
 }
--- src/main/java/id/ac/ui/cs/advprog/eshop/service/ProductServiceImpl.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/service/ProductServiceImpl.java
@@ -1,32 +1,25 @@
 package id.ac.ui.cs.advprog.eshop.service;
 import id.ac.ui.cs.advprog.eshop.model.Product;
-import id.ac.ui.cs.advprog.eshop.repository.ProductRepository;
+import id.ac.ui.cs.advprog.eshop.repository.ProductRepositoryImpl;
 import org.springframework.stereotype.Service;
-
-import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 
 
 @Service
 public class ProductServiceImpl implements ProductService {
-    private final ProductRepository productRepository;
+    private final ProductRepositoryImpl productRepository;
 
-    public ProductServiceImpl(ProductRepository productRepository) {
+    public ProductServiceImpl(ProductRepositoryImpl productRepository) {
         this.productRepository = productRepository;
     }
 
     @Override
     public Product create(Product product) {
-        productRepository.create(product);
-        return product;
+       return productRepository.create(product);
     }
     @Override
     public List<Product> findAll() {
-        Iterator<Product> productIterator = productRepository.findAll();
-        List<Product> allProduct = new ArrayList<>();
-        productIterator.forEachRemaining(allProduct::add);
-        return allProduct;
+        return productRepository.findAll();
     }
 
     @Override
