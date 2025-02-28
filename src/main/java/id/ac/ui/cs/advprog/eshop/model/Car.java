26cc4a1ee16b209770aa5d334131dd657a9763ae
dikbayy <dicky.bayusadewo@gmail.com>
2025-02-28 18:42:39 +0700

    Feat: Add validation to Car model

--- src/main/java/id/ac/ui/cs/advprog/eshop/model/Car.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/Car.java
@@ -1,11 +1,19 @@
 package id.ac.ui.cs.advprog.eshop.model;
 import lombok.Getter;
 import lombok.Setter;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Min;
 
 @Getter @Setter
 public class Car {
     private String carId;
+
+    @NotBlank(message = "Car name is required")
     private String carName;
+
+    @NotBlank(message = "Car color is required")
     private String carColor;
+
+    @Min(value = 1, message = "Car quantity must be at least 1")
     private int carQuantity;
 }
