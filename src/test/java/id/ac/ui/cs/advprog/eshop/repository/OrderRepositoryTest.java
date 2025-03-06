6458834e632273080357aa03ba5c110efcc3a690
dikbayy <dicky.bayusadewo@gmail.com>
2025-03-06 17:53:54 +0700

    Test: Add import packages for testing

--- src/test/java/id/ac/ui/cs/advprog/eshop/repository/OrderRepositoryTest.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/repository/OrderRepositoryTest.java
@@ -1,6 +1,14 @@
 package id.ac.ui.cs.advprog.eshop.repository;
 
 import org.junit.jupiter.api.BeforeEach;
+import id.ac.ui.cs.advprog.eshop.enums.OrderStatus;
+import id.ac.ui.cs.advprog.eshop.model.Order;
+import id.ac.ui.cs.advprog.eshop.model.Product;
+import org.junit.jupiter.api.Test;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 class OrderRepositoryTest {
 
