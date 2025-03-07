f4f59f4f9b5d8beae363d3d0f907a51ba5d507d1
dikbayy <dicky.bayusadewo@gmail.com>
2025-03-07 20:25:17 +0700

    [GREEN] Implement PaymentRepository class

--- src/main/java/id/ac/ui/cs/advprog/eshop/repository/PaymentRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/repository/PaymentRepository.java
@@ -4,30 +4,61 @@ import id.ac.ui.cs.advprog.eshop.model.Payment;
 import id.ac.ui.cs.advprog.eshop.model.Order;
 import org.springframework.stereotype.Repository;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 @Repository
 public class PaymentRepository {
-    private List<Payment> payments;
-    private Map<String, Order> paymentOrder;
+    private List<Payment> paymentData = new ArrayList<>();
+    private Map<String, Order> paymentOrder = new HashMap<>();
 
     public Payment save(Order order, Payment payment) {
-        return null;
+        paymentData.add(payment);
+        paymentOrder.put(payment.getId(), order);
+        if (payment.getStatus().equals("SUCCESS")) {
+            order.setStatus("SUCCESS");
+        } else if (payment.getStatus().equals("REJECTED")) {
+            order.setStatus("FAILED");
+        } else {
+            throw new IllegalArgumentException();
+        }
+
+        return payment;
     }
 
     public void update(Payment payment, String status) {
+        Order order = this.getOrder(payment.getId());
+        payment.setStatus(status);
+        if (status.equals("SUCCESS")) {
+            order.setStatus("SUCCESS");
+        } else if (status.equals("REJECTED")) {
+            order.setStatus("FAILED");
+        } else {
+            throw new IllegalArgumentException();
+        }
     }
 
     public Payment findById(String paymentId) {
+        for (Payment savedPayment : paymentData) {
+            if (savedPayment.getId().equals(paymentId)) {
+                return savedPayment;
+            }
+        }
         return null;
     }
 
     public List<Payment> findAll() {
-        return null;
+        List<Payment> allPayments = new ArrayList<>();
+        for (Payment payment : paymentData) {
+            allPayments.add(payment);
+        }
+
+        return allPayments;
     }
 
     public Order getOrder(String paymentId) {
-        return null;
+        return paymentOrder.get(paymentId);
     }
 }
